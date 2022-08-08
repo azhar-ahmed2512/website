087e3d166daf7ea4fbedf8950b3ff6d63a740850
anas <anas203044@gmail.com>
2022-08-08 20:24:26 +0500

    Added First Website Content

---
+++ js/app.js
@@ -0,0 +1,60 @@
+/* Set Date */
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
+
+/* nav toggle */
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+
+console.log(navBtn);
+console.log(links);
+
+//add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+//nav fixed
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 62) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
+
+// smooth scroll
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    links.classList.remove("show-links");
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //position
+    let position;
+    if (navbar.classList.contains("fixed")) {
+      position = element.offsetTop - 62;
+    } else {
+      position = element.offsetTop - 124;
+    }
+    if (window.innerWidth < 992) {
+      if (navbar.classList.contains("fixed")) {
+        position = element.offsetTop - 62;
+      }
+      position = element.offsetTop - 62;
+    }
+    //window scrolltop
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth",
+    });
+  });
+});
